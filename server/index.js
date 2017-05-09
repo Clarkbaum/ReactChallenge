dbdf4e22bdc9dd9607f8635bf5274e05f777437f
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-05-09 15:20:16 -0500

    npm start

--- server/index.js
+++ server/index.js
@@ -4,3 +4,19 @@ var bodyParser = require('body-parser');
 
 const app = express();
 module.exports = app;
+
+app.post('/', function(req, res){
+
+})
+
+app.get('/', function(req, res){
+  res.send("get /")
+})
+
+app.post('/', function(req, res){
+
+})
+
+app.listen('3000', function(){
+  console.log('listening on port 3000');
+})
