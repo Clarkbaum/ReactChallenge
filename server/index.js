e8529e99a79afbc505b5cb9fd120d04adb9a6439
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-05-10 12:26:34 -0500

    working on mongo

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
-var bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const bodyParser = require('body-parser');
 
 const app = express();
 module.exports = app;
+mongoose.Promise = Promise;
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/react-challenge')
+.then(() => console.log('Database connected'))
+.catch(() => console.error('Database failed to connect'));
+
 
 app.post('/', function(req, res){
   console.log("req.body", req.body)
