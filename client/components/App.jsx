198ac566ac9b6023d90cd9bf402c0b0b06dd4595
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-05-15 15:19:42 -0500

    add article state set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,8 @@ import 'whatwg-fetch';
 import Article from './Article'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
 
 
 class App extends React.Component {
@@ -10,12 +12,29 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      data: []
+      data: [],
+      open: false,
+      addTitle: 'title',
+      addAuthor: 'author',
+      addDate: 'date',
+      addBody: 'body'
     };
   }
   componentDidMount() {
     this.getArticles();
   }
+
+  handleOpen() {
+    this.setState({open: true});
+  };
+
+  handleClose() {
+    this.setState({open: false});
+    this.setState({addTitle: 'title'})
+    this.setState({addAuthor: 'author'})
+    this.setState({addDate: 'date'})
+    this.setState({addBody: 'body'})
+  };
 
   getArticles() {
     fetch('http://localhost:8000/articles', {
@@ -31,13 +50,82 @@ class App extends React.Component {
     .catch(err => console.log("error fetching articles", err))
   }
 
+  textChange(type, value) {
+    if(type === 'title'){
+      this.setState({addTitle: value})
+    } else if (type === 'author') {
+      this.setState({addAuthor: value})
+    } else if (type === 'date') {
+      this.setState({addDate: value})
+    } else if (type === 'body') {
+      this.setState({addBody: value})
+    }
+  }
+
+  addArticle() {
+
+  }
+
   render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose.bind(this)}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        onTouchTap={this.addArticle.bind(this)}
+      />,
+    ];
     return (
       <MuiThemeProvider>
         <div>
           <h1>React Full Stack </h1>
-          <FlatButton label="add article" />
-
+          <FlatButton 
+            label="Add Article" 
+            onTouchTap={this.handleOpen.bind(this)}
+          />
+          <Dialog 
+            title="Add Article"
+            open={this.state.open}
+            actions={actions}
+          >
+            <div>
+              <TextField
+                name='title'
+                floatingLabelText='title'
+                fullWidth={true}
+                onChange={(e, value) => this.textChange('title', value)}
+              />
+            </div>
+            <div>
+              <TextField
+                name='author'
+                floatingLabelText='author'
+                fullWidth={true}
+                onChange={(e, value) => this.textChange('author', value)}
+              />
+            </div>
+            <div>
+              <TextField
+                name='date'
+                floatingLabelText='date'
+                fullWidth={true}
+                onChange={(e, value) => this.textChange('date', value)}
+              />
+            </div>
+            <div>
+              <TextField
+                name='articalBody'
+                floatingLabelText='articalBody'
+                multiLine
+                fullWidth={true}
+                onChange={(e, value) => this.textChange('body', value)}
+              />
+            </div>
+          </Dialog>
           {this.state.data.map(article => 
             <Article article={article}/>
           )}
